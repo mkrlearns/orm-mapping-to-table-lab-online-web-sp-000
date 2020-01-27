62704891707c632761ae2291e801116f4289830d
ryan m <contact@ryanmeek.com>
2020-01-27 00:26:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,9 +19,6 @@ class Student
   def self.drop_table() DB[:conn].execute("DROP TABLE students") end
 
   def save
-    sql = <<~SQL
-      INSERT INTO students (name, grade) VALUES (?, ?)
-    SQL
     DB[:conn].execute('INSERT INTO students (name, grade) VALUES (?, ?)', self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
